cd7c86c97e2583b8ecac4f3f487e19ad92f036e3
shubhashishneutroline <shubhashish.neutroline@gmail.com>
2025-07-04 14:30:44 +0545

    Update route.ts

--- src/app/api/langchain/route.ts
+++ src/app/api/langchain/route.ts
@@ -298,7 +298,7 @@ export async function POST(req: NextRequest) {
     const llm = new ChatOpenAI({
       model: "deepseek/deepseek-r1:free",
       openAIApiKey:
-        "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",
+       process.env.API_KEY,
       configuration: { baseURL: "https://openrouter.ai/api/v1" },
       temperature: 0,
       streaming: false,
